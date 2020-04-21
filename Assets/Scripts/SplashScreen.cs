83b3345a8151d9ff2c1de054d05f687b5a47ed8b
jpparent <jeanp.parent@gmail.com>
2020-04-20 20:51:08 -0400

    Add splash screen and elevated tilemap look

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+	public int m_nextSceneToLoad = 1;
+
+	public void LoadNextScene()
+	{
+		SceneManager.LoadScene( m_nextSceneToLoad );
+	}
+}
